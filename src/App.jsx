8a9f29549a5d288b58d6921f9e197dfdce9c7556
krishna1632 <krishna163208@gmail.com>
2024-10-01 16:27:31 +0530

    fghjkl

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-import React from "react"
-function App(){
-  return
-        <React.Fragment>
-
-        </React.Fragment>
+function App() {
+  return (
+    
+    <h1>Hello</h1> 
+  )
 }
 
 export default App;
