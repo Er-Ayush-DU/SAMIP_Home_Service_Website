8ccbb724af3ecb7736d687f805e6c13a794ef7f0
pra8953 <prabhatkumar6920@gmail.com>
2024-10-02 02:29:34 +0530

    dfghjk

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,37 @@
-
-
+import React from "react";
+import TopHeader from "./Components/TopHeader";
+import Hero from "./Components/Hero";
+import Contact from "./Components/Contact";
+import Footer from "./Components/Footer";
+import AboutUs from "./Components/AboutUs.jsx";
+import Goals from "./components/Goals";
+import Total from "./components/Total";
+import Feature from "./Components/Feature.jsx";
+import Testimonial from "./components/Testimonial";
+import FAQ from "./Components/FAQ.jsx";
 
 function App() {
-  return (<h1>Ram</h1>)
+  return <React.Fragment>
+            <header>
+            <TopHeader></TopHeader>
+            <Hero></Hero>
+            </header>
+            <main>
+            <AboutUs></AboutUs>
+            <Goals></Goals>
+            <Total></Total>
+            <Feature></Feature>
+            <Testimonial></Testimonial>
+            <Contact></Contact>
+            <FAQ></FAQ>
+     
+            </main>
+
+            
+           <footer>
+           <Footer></Footer>
+           </footer>
+          </React.Fragment>
 
 }
 
