5f6a42c01ee5be26cc0a82f1fe2550f25248d764
Er-Ayush-DU <er.ayush336@gmail.com>
2024-10-01 16:28:31 +0530

    ahdb

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from "react"
-function App(){
-  return
-        <React.Fragment>
-
-        </React.Fragment>
+function App() {
+  return (
+    <React.Fragment>
+      <h1>Hello  samip</h1>
+    </React.Fragment>
+  )
 }
 
 export default App;
