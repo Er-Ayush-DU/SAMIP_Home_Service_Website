022130874ae50c8966f21d05f0f99d2535537358
Er-Ayush-DU <er.ayush336@gmail.com>
2024-10-04 22:55:21 +0530

    AAj ka majdoori completed by ayush tiwari

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -11,46 +11,46 @@ import { FaXTwitter } from "react-icons/fa6";
 function Footer() {
     return (
         <React.Fragment>
-            <div className="h-[15rem] bg-[#0c0b0b] text-white p-5">
-                <div className="flex justify-between">
+            <div className=" overflow-hidden h-[15rem] bg-[#0c0b0b] text-white p-5">
+                <div className="h-[250px] overflow-hidden flex justify-between">
                     {/* Logo Section */}
-                    <div className="h-[11rem] ">
+                    <div className="FooterLogo h-[11rem] ">
                         <img className="h-[11rem]" src="src/assets/Images/samip-logo2.png" alt="Logo" />
                     </div>
 
                     {/* Contact Section */}
-                    <div className="flex flex-col gap-2 justify-center">
-                       <div>
-                                <div className="flex  items-center">
-                                        <span className="mr-2 flex "><IoMdMail /></span>
-                                        info@lokutthanfoundation.in
-                                    </div>
-                                    <div className="flex  items-center">
-                                        <span className="mr-2"><FaPhone /></span>
-                                        +91 01117479993
-                                    </div>
-                                    <div className="flex  items-center">
-                                        <span className="mr-2"><FaWhatsapp /></span>
-                                        +91 8076218842
-                                    </div>
-                                    <div className="flex  items-center">
-                                        <span className="mr-2"><FaMapMarkerAlt /></span>
-                                        N-18, Kalkaji, New Delhi 110019
-                                    </div>
-                                </div>
-                                <div className="flex mt-7 text-[1.5rem] justify-between">
-                               <a href="#"> <BsInstagram /></a>
-                               <a href="#"> <FaFacebookF /></a>
-                               <a href="#">   <FaLinkedinIn /></a>
-                               <a href="#"> <FaXTwitter /></a>
-                               
-                             
-                               
-                                </div>
+                    <div className="Footer_Cont_Media flex flex-col gap-2 justify-center">
+                        <div className="FooterContact">
+                            <div className="flex  items-center">
+                                <span className="mr-2 flex "><IoMdMail /></span>
+                                info@lokutthanfoundation.in
+                            </div>
+                            <div className="flex  items-center">
+                                <span className="mr-2"><FaPhone /></span>
+                                +91 01117479993
+                            </div>
+                            <div className="flex  items-center">
+                                <span className="mr-2"><FaWhatsapp /></span>
+                                +91 8076218842
+                            </div>
+                            <div className="flex  items-center">
+                                <span className="mr-2"><FaMapMarkerAlt /></span>
+                                N-18, Kalkaji, New Delhi 110019
+                            </div>
+                        </div>
+                        <div className="FooterSocial flex mt-7 text-[1.5rem] justify-between">
+                            <a href="#"> <BsInstagram /></a>
+                            <a href="#"> <FaFacebookF /></a>
+                            <a href="#">   <FaLinkedinIn /></a>
+                            <a href="#"> <FaXTwitter /></a>
+
+
+
+                        </div>
                     </div>
 
                     {/* Quick Links */}
-                    <div className="flex flex-col justify-center">
+                    <div className="QuickLinks flex flex-col justify-center">
                         <div className="font-bold mb-2 ">Quick Links</div>
                         <a href="https://lokutthanfoundation.in/" className="hover:underline">@Lok Utthan Pehal</a>
                         <a href="https://manacleindia.com/" className="hover:underline">@Manacle Technologies</a>
@@ -58,7 +58,7 @@ function Footer() {
                     </div>
 
                     {/* Google Maps Iframe Section */}
-                    <div className="iframe w-[400px] h-[200px]">
+                    <div className="GoogleMap iframe w-[400px] h-[200px]">
                         <iframe
                             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d224346.42715646436!2d77.06889735283102!3d28.52728034328008!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390ce3f114b2d91d%3A0x8e2a1e9ad54bc048!2sKalkaji%2C%20New%20Delhi%2C%20Delhi!5e0!3m2!1sen!2sin!4v1633045783913!5m2!1sen!2sin"
                             width="400"
@@ -70,7 +70,7 @@ function Footer() {
                     </div>
                 </div>
             </div>
-            <div className="bg-[#504e4e] flex justify-center text-[#fff] font-semibold">
+            <div className="copyRights bg-[#504e4e] flex justify-center text-[#fff] font-semibold">
                 <div>
                     All Rights Reserved @LUP
                 </div>
